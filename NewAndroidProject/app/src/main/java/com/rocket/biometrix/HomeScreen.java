8e42948d47b6f8e748dc22ff9d3f7aa9e3a7fec9
JayGarxP <jpfallon4@gmail.com>
2016-03-07 20:02:42 -0800

    Got rid of bright colors to avoid startling engineers

--- NewAndroidProject/app/src/main/java/com/rocket/biometrix/HomeScreen.java
+++ NewAndroidProject/app/src/main/java/com/rocket/biometrix/HomeScreen.java
@@ -89,7 +89,6 @@ public class HomeScreen extends Fragment {
         return v;
     }
 
-
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
