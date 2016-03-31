12a72f36822c37396b2e8560d6138fa5243d4796
JayGarxP <jpfallon4@gmail.com>
2016-03-30 21:17:41 -0700

    Made attack plan; setup dev environment
    Only had 1 hour to work on this; wasted 30 min on getting A.S. up and
    running.

--- NewAndroidProject/app/src/main/java/com/rocket/biometrix/EditPastEntries/EditPastActivity.java
+++ NewAndroidProject/app/src/main/java/com/rocket/biometrix/EditPastEntries/EditPastActivity.java
@@ -15,6 +15,13 @@ public class EditPastActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //TODO: Get Calendar Fragment and RV fragment in here, implement their on frag interactions somehow
+        //TODO: USE this Activity as 'bridge'
+        //TODO: try http://developer.android.com/training/basics/fragments/communicating.html
+        //TODO: Get tests (can be hardcoded not true tests) running to demo current RV code
+        //https://developer.android.com/training/material/lists-cards.html
+        //TODO: FInally finish RV code and start adding tab code etc. in actually extensible way
+        //TODO: PRAY TO SWEET BABY JESUS that changes to the LocalDB don't ruin my life.
 
     }
 
