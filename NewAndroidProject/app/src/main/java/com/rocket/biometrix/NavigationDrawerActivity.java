e487c797bebb405c116b147989dd5f97eb5ed613
Troy Riblett <troy.riblett@oit.edu>
2016-04-23 09:50:26 -0700

    Settings button now chooses correct settings
    The settings button now chooses the correct settings fragment to open
    based on the active fragment.

--- NewAndroidProject/app/src/main/java/com/rocket/biometrix/NavigationDrawerActivity.java
+++ NewAndroidProject/app/src/main/java/com/rocket/biometrix/NavigationDrawerActivity.java
@@ -101,15 +101,52 @@ public class NavigationDrawerActivity extends AppCompatActivity
         return true;
     }
 
+    /**
+     * The method called when one of the items on the help side of the nav drawer is called
+     * @param item The item that was clicked on in order to call this event
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Fragment frag = new HomeScreen();
+        Fragment frag;
         switch(item.getItemId()) {
             case R.id.action_help:
 
                 return true;
             case R.id.action_settings:
-                frag = new ModuleSettings();
+                Class fragClass = activeFragment.getClass();
+
+                if(fragClass.equals(SleepParent.class) || fragClass.equals(SleepGraph.class)
+                        || fragClass.equals(SleepEntry.class))
+                {
+                    frag = new SleepSettings();
+                }
+                else if(fragClass.equals(MoodParent.class) || fragClass.equals(MoodGraph.class)
+                        || fragClass.equals(MoodEntry.class))
+                {
+                    frag = new MoodSettings();
+                }
+                else if(fragClass.equals(DietParent.class) //TODO: || fragClass.equals(DietGraph.class)
+                        || fragClass.equals(DietEntry.class))
+                {
+                    frag = new DietSettings();
+                }
+                else if(fragClass.equals(MedicationParent.class) //TODO: || fragClass.equals(MedicationGraph.class)
+                        || fragClass.equals(MedicationEntry.class))
+                {
+                    frag = new MedicationSettings();
+                }
+                else if(fragClass.equals(ExerciseParent.class) //TODO: || fragClass.equals(ExerciseGraph.class)
+                        || fragClass.equals(SleepEntry.class))
+                {
+                    frag = new ExerciseSettings();
+                }
+                else
+                {
+                    frag = new ModuleSettings();
+                }
+
+                replaceFragment(frag);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
